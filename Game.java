6ac1a5555573cb2d149be2d878ecc7f3a831235b
Peetu Särkkä <peetu.sarkka@cs.tamk.fi>
2014-10-23 19:17:37 +0300

    Adds javadoc commenting to Game.java.
    Adds javadoc commenting to Game.java.

--- Game.java
+++ Game.java
@@ -1,3 +1,9 @@
-class Game {
+/**
+ * Initiates the slot-machine game.
+ * 
+ * Initiates the slot-machine game. Goal of the game is
+ * to be lucky and have all three slots be the same type.
+ */
+public class Game {
 
 }
