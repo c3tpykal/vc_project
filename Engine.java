2a3d483f27edc574fce857bc6069d2b62a493c37
Mikael Järvinen <c3mjarvi@ohm.tamk.fi>
2014-10-23 19:59:09 +0300

    Adds initial loop-functionality.
    Simply creates a while-loop for future use.

--- Engine.java
+++ Engine.java
@@ -1,8 +1,27 @@
 /**
  * Contains the game mechanics.
- * 
+ *
  * Contains the game mechanics.
  */
 public class Engine {
 
+    private int[] fruits = {1, 2, 3};
+    private boolean gameIsOn = true;
+
+    /**
+     * Constructs an "engine" and calls the loop-method.
+     *
+     * Constructs an "engine" and calls the loop-method.
+     **/
+    public Engine() {
+        loop();
+    }
+
+    public void loop() {
+
+        while(gameIsOn) {
+
+            gameIsOn = false;
+        }
+    }
 }
