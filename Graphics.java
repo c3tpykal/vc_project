9770884fc16524a827e1e9a772b5c929983e75b6
Mikael Järvinen <c3mjarvi@ohm.tamk.fi>
2014-10-23 19:37:42 +0300

    Adds draw()-method.
    The draw()-method draws the game mostly using
    System.out.printf, so it looks a bit complicated.

--- Graphics.java
+++ Graphics.java
@@ -1,8 +1,22 @@
-/**
- * Contains a method to draw the slot machine.
- *
- * Contains a method to draw the slot machine.
- */
 public class Graphics {
 
+    public static void draw(int a, int b, int c) {
+        String border = "*";
+        int borderWidth = 15;
+
+        // top row
+        System.out.printf("%n" + "%s %s %s", border, border, border);
+        System.out.printf("%2s%2s%2s", border, border, border);
+        System.out.printf("%2s%2s%2s o", border, border, border);
+
+        // middle row
+        System.out.printf("%n" + "%s %d" + "%2s", border, a, border);
+        System.out.printf("%2s %d" + "%2s", border, b, border);
+        System.out.printf("%2s %d" + "%2s |", border, c, border);
+
+        // bottom row
+        System.out.printf("%n" + "%s %s %s", border, border, border);
+        System.out.printf("%2s%2s%2s", border, border, border);
+        System.out.printf("%2s%2s%2s" + "%n", border, border, border);
+    }
 }
