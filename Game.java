65968328ddd3f9ed2aefe2db873f28174f79aa86
Peetu Särkkä <peetu.sarkka@cs.tamk.fi>
2014-10-23 19:32:09 +0300

    Adds the main method to Game.java.
    Adds the main method to Game.java.
    Also adds the javadoc commeting to it.

--- Game.java
+++ Game.java
@@ -5,5 +5,14 @@
  * to be lucky and have all three slots be the same type.
  */
 public class Game {
-
+       /**
+        * Initiates the slot-machine game.
+        * 
+        * Initiates the slot-machine game.
+        *
+        * @param args Command line parameters. Not used.
+        */
+	public static void main(String[] args) {
+        Engine fruitGame = new Engine();
+    }
 }
